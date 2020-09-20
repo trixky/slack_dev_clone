d3f373536a0aad88e7ace91e07074fabd6310aad
Mathis Bois <mbois@zaion.ai>
2020-09-20 12:01:22 +0200

    add the avatar

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -4,6 +4,7 @@ import { Menu } from 'semantic-ui-react';
 
 class SidePanel extends Component {
 	render() {
+		const { currentUser } = this.props;
 		return (
 			<Menu
 				size="large"
@@ -11,7 +12,7 @@ class SidePanel extends Component {
 				fixed="left"
 				vertical
 				style={{ background: '#4c3c4c', fontSize: '1.2rem' }}>
-				<UserPanel />
+				<UserPanel currentUser={currentUser} />
 			</Menu>
 		)
 	}
--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import firebase from '../../firebase';
-import { Grid, Header, Icon, Dropdown} from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
 
 class UserPanel extends Component {
+	state = {
+		user: this.props.currentUser
+	}
+
 	dropdownOptions = _ => [
 		{
 			key: 'user',
-			text: <span>Signed in as <strong>User</strong></span>,
+			text: <span>Signed in as <strong>{this.state.user.displayName}</strong></span>,
 			disabled: true
 		},
 		{
@@ -21,12 +25,13 @@ class UserPanel extends Component {
 
 	handleSignout = _ => {
 		firebase
-		.auth()
-		.signOut()
-		.then(_ => console.log('signed out!'))
+			.auth()
+			.signOut()
+			.then(_ => console.log('signed out!'))
 	}
 
 	render() {
+		const { user } = this.state;
 		return (
 			<Grid style={{ background: "#4c3c4c" }}>
 				<Grid.Column>
@@ -36,13 +41,16 @@ class UserPanel extends Component {
 							<Icon name="code" />
 							<Header.Content>DevChat</Header.Content>
 						</Header>
+						{/* User Dropdown */}
+						<Header style={{ padding: '0.25em' }} as="h4" inverted>
+							<Dropdown trigger={
+								<span>
+									<Image src={user.photoURL} spaced="right" avatar />
+									{user.displayName}
+								</span>
+							} options={this.dropdownOptions()} />
+						</Header>
 					</Grid.Row>
-					{/* User Dropdown */}
-					<Header style={{padding: '0.25em' }} as="h4" inverted>
-						<Dropdown trigger={
-							<span>User</span>
-						} options={this.dropdownOptions()}/>
-					</Header>
 				</Grid.Column>
 			</Grid>
 		);
