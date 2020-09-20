dada33c4c4767d79c9f8a1c0d31ecafa9a677fa1
Mathis Bois <mbois@zaion.ai>
2020-09-20 20:41:13 +0200

    start to displaying messages on public channels

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {Segment, Comment} from 'semantic-ui-react';
+
+import MessageHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
 
 class Messages extends Component {
 	render() {
 		return (
-			<div>Messages</div>
+			<Fragment>
+				<MessageHeader />
+				<Segment>
+					<Comment.Group className="messages">
+						{/* Messages */}
+					</Comment.Group>
+				</Segment>
+				<MessageForm />
+			</Fragment>
 		)
 	}
 }
