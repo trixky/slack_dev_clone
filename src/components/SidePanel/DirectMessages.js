b97874a21f4d784b8ebe94d5f5225a97a7878abd
Mathis Bois <mbois@zaion.ai>
2020-09-23 08:06:35 +0200

    start private channel

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import firebase from '../../firebase';
+import { connect } from 'react-redux';
+import { setCurrentChannel, setPrivateChannel } from '../../actions';
 import { Menu, Icon } from 'semantic-ui-react';
 
 class DirectMessages extends Component {
@@ -68,6 +70,24 @@ class DirectMessages extends Component {
 
 	isUserOnline = user => user.status === 'online';
 
+	changeChannel = user => {
+		const channelId = this.getChannelId(user.uid);
+		const channelData = {
+			id: channelId,
+			name: user.name
+		};
+
+		this.props.setCurrentChannel(channelData);
+		this.props.setPrivateChannel(true);
+	}
+
+	getChannelId = userId => {
+		const currentUserId = this.state.user.uid;
+		return userId < currentUserId ?
+			`${userId}/${currentUserId}` :
+			`${currentUserId}/${userId}`
+	}
+
 	render() {
 		const { users } = this.state;
 		return (
@@ -81,7 +101,7 @@ class DirectMessages extends Component {
 				{users.map(user => (
 					<Menu.Item
 						key={user.uid}
-						onClick={_ => console.log(user)}
+						onClick={_ => this.changeChannel(user)}
 						style={{ opacity: 0.7, fontStyle: 'italic' }}
 					>
 						<Icon
@@ -96,4 +116,7 @@ class DirectMessages extends Component {
 	}
 }
 
-export default DirectMessages;
+export default connect(
+	null,
+	{ setCurrentChannel, setPrivateChannel }
+)(DirectMessages);
